dbedca678d255818226e38e83b20421fb5f8cdbc
Michael Engelmann <zeprogramfactory@outlook.com>
2019-06-30 23:57:15 +0200

    Initial checkin ...

---
+++ XFTestWPF/XFTestWPF/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFTestWPF
+{
+   // Learn more about making custom code visible in the Xamarin.Forms previewer
+   // by visiting https://aka.ms/xamarinforms-previewer
+   [DesignTimeVisible(true)]
+   public partial class MainPage : ContentPage
+   {
+      public MainPage()
+      {
+         InitializeComponent();
+
+         image.Source = ImageSource.FromResource("XFTestWPF.Image.Aim4-WF.png", typeof(MainPage));
+      }
+   }
+}
